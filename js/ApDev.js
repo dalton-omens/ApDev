44ca8d1e4df654248940357d5ef7919d5b8a6bf0
Dalton <daltonomens@berkeley.edu>
2017-07-28 20:38:07 -0700

    add animations to services page

---
+++ js/ApDev.js
@@ -0,0 +1,46 @@
+if (typeof jQuery === 'undefined') {
+  throw new Error('ApDev Javascript requires jQuery')
+}
+
+//ANIMATIONS
+$(function() {
+
+  var $window           = $(window),
+      win_height_padded = $window.height() * 1.1;
+      //isTouch           = Modernizr.touch;
+
+  //if (isTouch) { $('.revealOnScroll').addClass('animated');}
+
+  $window.on('scroll', revealOnScroll);
+
+  function revealOnScroll() {
+    var scrolled = $window.scrollTop(),
+        win_height_padded = $window.height() * 1.1;
+
+    // Showed...
+    $(".revealOnScroll:not(.animated)").each(function () {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+
+      if (scrolled + win_height_padded > offsetTop + 300) {
+        if ($this.data('timeout')) {
+          window.setTimeout(function(){
+            $this.addClass('animated ' + $this.data('animation'));
+          }, parseInt($this.data('timeout'),10));
+        } else {
+          $this.addClass('animated ' + $this.data('animation'));
+        }
+      }
+    });
+    // Hidden...
+   $(".revealOnScroll.animated").each(function (index) {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+      if (scrolled + win_height_padded < offsetTop) {
+        $(this).removeClass('animated slide-in-left slide-in-right')
+      }
+    });
+  }
+
+  revealOnScroll();
+});
